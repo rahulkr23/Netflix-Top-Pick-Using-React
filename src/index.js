fd59963f3dbd8a08c07a4039aa198ff00c699f8b
rahulkr23 <rahulwhy23@gmail.com>
2021-04-10 11:24:44 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <>
+  <h1>Rahul Kumar Netflix Top Series</h1>
+
+  <p> Here The List Of My Feb 5 Netflix Series </p>
+  <ol>
+    <li>OJack Horseman. </li>
+    <li>Stranger Things.</li>
+    <li>Ozark.</li>
+    <li>The Crown. </li>
+    <li>The Witcher.</li>
+  </ol>
+  </>,
+
+  document.getElementById('root')
+  
+);
